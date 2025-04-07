92721a75256592b7290bfa0cf0b7290e8ed06e5f
Toyotatech01 <edwinonsongo255@gmail.com>
2025-04-07 04:01:42 +0300

    Update yt-search.js

--- action/yt-search.js
+++ action/yt-search.js
@@ -19,9 +19,9 @@ zokou({ nomCom: "yts", categorie: "Search", reaction: "💎" }, async (dest, zk,
 
     let captions = "";
 for (let i = 0; i < 15; i++) {
-  captions += `╭━━Σ.И.ß-XM𝐃-𝚩𝚯T YOUTUBE MENU━━╮\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
+  captions += `╭━━SCORPION-V8 YOUTUBE MENU━━╮\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
 }
-    captions += "\n======\n*powered by Σ.И.ß-XM𝐃-𝚩𝚯T*";
+    captions += "\n======\n*powered by SCORPION-V8*";
 
     // repondre(captions)
     zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
@@ -77,7 +77,7 @@ _*En cours de téléchargement...*_\n\n`
 
     fileStream.on('finish', () => {
       // Envoi du fichier vidéo en utilisant l'URL du fichier local
-      zk.sendMessage(origineMessage, { video: { url: `./${filename}` }, caption: "Powered by *Σ.И.ß-XM𝐃-𝚩𝚯T*", gifPlayback: false }, { quoted: ms });
+      zk.sendMessage(origineMessage, { video: { url: `./${filename}` }, caption: "Powered by *SCORPION-V8*", gifPlayback: false }, { quoted: ms });
 
     });
 
